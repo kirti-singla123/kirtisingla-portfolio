ce4d90c0d15e675a5968529f993ff50275ac39f0
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-12 12:49:25 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,7 @@ const Home = () => {
             <div className="relative animate-slide-up">
               <div className="relative rounded-2xl overflow-hidden shadow-elegant">
                 <img 
-                  src={hero-image.jpg} 
+                  src={heroImage} 
                   alt="Professional workspace" 
                   className="w-full h-auto object-cover"
                 />
