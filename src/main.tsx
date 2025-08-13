e9aa7c67b2b91b9bdd6839d468abee72b7452f6f
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-13 10:59:09 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Navbar-component from "./components/ui/Navbar-component";
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
