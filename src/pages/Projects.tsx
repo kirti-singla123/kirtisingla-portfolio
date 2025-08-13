6ae5252c8fe15b1fb39f34d0091baa95529a5f32
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-13 11:26:01 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import { ExternalLink } from "lucide-react";
 
 const Projects = () => {
